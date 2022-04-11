199e135f7d4c169f42f28ff8af90d0bc81d6fd9e
themostluckytina <themostluckytina@gmail.com>
2022-04-11 22:39:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
 function App() {
-  return (
+
+  const [ plants, setPlants] = useState(data);
+
+const remove =(id)=>{
+  
+  let newPlants = plants.filter(flower => flower.id !== id);
+ setPlants(newPlants)
+    
+}
+  
+
+return ( <div>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <h4>ОНЛАЙН МАГАЗИН</h4>
+      <h2> САДОВАЯ ФЕРМА</h2>
+      </div>
+
+<div className="App">
+
+  <h1>{plants.length} НОВЫХ ПОСТУПЛЕНИЙ САЖЕНЦЕВ </h1>
+
+  {plants.map((plant => {
+    const{id, name, sort, image} = plant;
+
+    return( <div key={id}>
+
+<div  className="list">
+
+  <div> <p>{id} - {name} </p></div>
+  <div> <p className='sort'>{sort} </p> </div>
+  <div> <img src={image} width={250} alt='pic' /></div>
+ </div>
+
+<div>
+  <button className='btn' onClick={()=>remove(id)} >Удалить</button>
+</div>
+
+    </div>)
+  }))}
+<br></br>
+
+<div>
+  <button className='btnall' onClick={()=>setPlants([])} >Удалить все</button>
+</div>
+</div>
+
+
+
+</div>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const data = [
+
+    {
+        id: 1,
+        name: 'пион',
+        sort: 'Кармен',
+        image: 'https://img-fotki.yandex.ru/get/9310/32190361.0/0_bed39_773a65f_XL.jpg',
+    },
+    {
+        id:2,
+        name: 'пион',
+        sort: 'Вестсайдская история',
+        image: 'http://www.sadovod.com.ru/wa-data/public/shop/products/41/03/10341/images/580/580.750x0.jpg',
+    },
+    {
+        id:3,
+        name: 'пион',
+        sort: 'Зорька',
+        image: 'https://vosadu-li-vogorode.ru/wp-content/uploads/a/1/0/a107f9b402ba72084f0b03b6aa492ecf.jpeg',
+    },
+    {
+        id: 4,
+        name: 'роза',
+        sort: 'Ночная серенада',
+        image: 'https://domashniecvety.ru/wp-content/uploads/c/2/a/c2ac43002e33b68542f5d56a95688afa.jpe',
+    },
+    {
+        id: 5,
+        name: 'роза',
+        sort: 'Танго',
+        image: 'https://2sotki.ru/wp-content/uploads/c/9/6/c96177c0e06ffed7365d189bf8a8d920.jpg',
+    },
+    {
+        id: 6,
+        name: 'роза',
+        sort: 'Нежность',
+        image: 'https://2sotki.ru/wp-content/uploads/d/d/5/dd5954c67f1950273212acf70b49cd1b.jpg',
+    },
+    {
+        id: 7,
+        name: 'роза ',
+        sort: 'Улыбка Джоконды',
+        image: 'https://img-fotki.yandex.ru/get/5811/129389806.2/0_63c96_c27cbb18_XL.jpg',
+    },
+    {
+        id: 8,
+        name: 'флокс',
+        sort: 'Зазнайка',
+        image: 'https://domashniecvety.ru/wp-content/uploads/7/8/7/78710a30309f01c4a4dbf7536f49cadb.jpe',
+    },
+    {
+        id: 9,
+        name: 'флокс',
+        sort: 'Болеро',
+        image: 'https://i.pinimg.com/originals/ef/63/67/ef6367025c4be54a04fc267da8b0d733.jpg',
+    }
+
+
+
+]
